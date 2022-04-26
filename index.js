c3313c02651e401dc690ee4a8a936b22f95a3d47
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-04-25 17:28:53 -0700

    commit tree 4c24f02bf9db855fd0f8801542db80bca5beb92d

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400) {
+    return "This one is on me!"
+  } else if (rideLength > 400 && rideLength < 2000) {
+    return "That will be twenty bucks."
+  } else if (rideLength >= 2000 && rideLength <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
